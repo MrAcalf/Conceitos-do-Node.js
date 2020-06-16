78ff57617acb2f0cde3caf65560abcb805c1abc2
mracalf <antonio.lima16@fatec.sp.gov.br>
2020-06-16 13:45:14 -0300

    desafio Rocket Seat Conceitos de Node Completo

--- src/app.js
+++ src/app.js
@@ -1,33 +1,86 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4")
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
+})
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  repositories.push(repository);
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  return res.json(repository)
+})
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params
+  const { title, url, techs } = req.body
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
 
-module.exports = app;
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repository not found!'})
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return res.json(repository)
+})
+
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({ error: 'Repository not found!'})
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+  return res.status(204).send()
+})
+
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+  
+  if(repositoryIndex < 0){
+    return res.status(400).send()
+  }
+  const { title, url, techs, likes } = repositories[repositoryIndex]
+  const addRepositoryLikes = likes + 1
+
+  const repositoryUpdated = { 
+    id, 
+    title, 
+    url, 
+    techs, 
+    likes: addRepositoryLikes 
+  }
+  
+
+  repositories[repositoryIndex] = repositoryUpdated
+
+  return res.json(repositoryUpdated)
+})
+
+module.exports = app
